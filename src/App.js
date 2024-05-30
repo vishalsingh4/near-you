76f5d808452b44ab68460e356d67cc115144c4a3
Vishal Singh <vishalsingh@MacBook-Pro.local>
2024-05-30 06:02:12 +0530

    deployment fixes with linting

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.scss'
 import HomePage from './pages/HomePage'
 
 function App() {
-  return (
-    <div className="app-container">
-      <HomePage />
-    </div>
-  )
+   return (
+      <div className="app-container">
+         <HomePage />
+      </div>
+   )
 }
 
 export default App
--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,6 @@ import { render } from '@testing-library/react'
 import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />)
-  expect(1).toBe(1)
+   render(<App />)
+   expect(1).toBe(1)
 })
