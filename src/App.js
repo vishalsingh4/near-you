2e6aaffe0a731e28c9585bbabf1bd5cdca3540ad
Vishal Singh <vishalsingh@MacBook-Pro.local>
2024-05-30 05:44:35 +0530

    CRA boiler plate added with coding standard setup - material ui, eslint, prettier, husky, lint-staged

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss'
+
+import HomePage from './pages/HomePage'
+
+function App() {
+  return (
+    <div className="app-container">
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
